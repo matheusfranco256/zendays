afe966e77e2e9dd4bfdb395a9010da9e934d8946
MatheusFranco <matheus.franco@evolutionsoft.com.br>
2023-06-23 14:10:02 -0300

    alteração program

--- src/ZenDays.API/Program.cs
+++ src/ZenDays.API/Program.cs
@@ -54,7 +54,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 //app.UseRouting();
 app.UseCors("cors");
-
+//app.UseAuthentication();
 app.UseMiddleware<JwtMiddleware>();
 
 app.UseMiddleware(typeof(HandlingMiddleware));
