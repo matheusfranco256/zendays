4b09cf00062fbd379f70e68bc17774758e76f512
Lucas V. <lucaas.vgp@gmail.com>
2024-04-12 10:26:18 -0300

    feat: altera banco de dados para o email do trabalho.

--- src/ZenDays.API/Controllers/DepartamentoController.cs
+++ src/ZenDays.API/Controllers/DepartamentoController.cs
@@ -1,5 +1,4 @@
 ﻿using AutoMapper;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ZenDays.Core.Models;
 using ZenDays.Service.DTO;
@@ -7,55 +6,55 @@ using ZenDays.Service.Interfaces;
 
 namespace ZenDays.API.Controllers
 {
-    [Route("api/v1/Departamento")]
-    [ApiController]
-    [Authorize(AuthenticationSchemes = "Bearer")]
-    public class DepartamentoController : BaseController
-    {
-        private readonly IMapper _mapper;
-        private readonly IDepartamentoService _departamentoService;
+	[Route("api/v1/Departamento")]
+	[ApiController]
+	//[Authorize(AuthenticationSchemes = "Bearer")]
+	public class DepartamentoController : BaseController
+	{
+		private readonly IMapper _mapper;
+		private readonly IDepartamentoService _departamentoService;
 
-        public DepartamentoController(IMapper mapper, IDepartamentoService departamentoService)
-        {
-            _mapper = mapper;
-            _departamentoService = departamentoService;
-        }
+		public DepartamentoController(IMapper mapper, IDepartamentoService departamentoService)
+		{
+			_mapper = mapper;
+			_departamentoService = departamentoService;
+		}
 
-        [HttpPost("Register")]
-        public async Task<IActionResult> Register([FromBody] RegisterDepartamentoViewModel model)
-        {
-            return Result(await _departamentoService.CreateDepartamento(_mapper.Map<DepartamentoDTO>(model)));
-        }
+		[HttpPost("Register")]
+		public async Task<IActionResult> Register([FromBody] RegisterDepartamentoViewModel model)
+		{
+			return Result(await _departamentoService.CreateDepartamento(_mapper.Map<DepartamentoDTO>(model)));
+		}
 
-        [HttpPut("Update")]
-        public async Task<IActionResult> Update([FromBody] UpdateDepartamentoViewModel model)
-        {
-            return Result(await _departamentoService.UpdateDepartamento(_mapper.Map<DepartamentoDTO>(model)));
-        }
-
-
-        [HttpGet("GetById")]
-        public async Task<IActionResult> Get(string id)
-        {
-            return Result(await _departamentoService.Get(id));
-        }
-        [HttpGet("GetByName")]
-        public async Task<IActionResult> GetByName(string name)
-        {
-            return Result(await _departamentoService.GetByName(name));
-        }
+		[HttpPut("Update")]
+		public async Task<IActionResult> Update([FromBody] UpdateDepartamentoViewModel model)
+		{
+			return Result(await _departamentoService.UpdateDepartamento(_mapper.Map<DepartamentoDTO>(model)));
+		}
 
 
-        [HttpGet("GetAll")]
-        public async Task<IActionResult> GetAll()
-        {
-            return Result(await _departamentoService.GetAll());
-        }
+		[HttpGet("GetById")]
+		public async Task<IActionResult> Get(string id)
+		{
+			return Result(await _departamentoService.Get(id));
+		}
+		[HttpGet("GetByName")]
+		public async Task<IActionResult> GetByName(string name)
+		{
+			return Result(await _departamentoService.GetByName(name));
+		}
 
-        [HttpDelete("Disable")]
-        public async Task<IActionResult> Delete(string id)
-        {
-            return Result(await _departamentoService.DisableDepartamento(id));
-        }
-    }
+
+		[HttpGet("GetAll")]
+		public async Task<IActionResult> GetAll()
+		{
+			return Result(await _departamentoService.GetAll());
+		}
+
+		[HttpDelete("Disable")]
+		public async Task<IActionResult> Delete(string id)
+		{
+			return Result(await _departamentoService.DisableDepartamento(id));
+		}
+	}
 }
--- src/ZenDays.API/Controllers/FeriasController.cs
+++ src/ZenDays.API/Controllers/FeriasController.cs
@@ -1,5 +1,4 @@
 ﻿using AutoMapper;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ZenDays.Core.Models;
 using ZenDays.Core.Utilities;
@@ -8,56 +7,56 @@ using ZenDays.Service.Interfaces;
 
 namespace ZenDays.API.Controllers
 {
-    [Route("api/v1/Ferias")]
-    [ApiController]
-    [Authorize(AuthenticationSchemes = "Bearer")]
-    public class FeriasController : BaseController
-    {
-        private readonly IMapper _mapper;
-        private readonly IFeriasService _feriasService;
+	[Route("api/v1/Ferias")]
+	[ApiController]
+	//[Authorize(AuthenticationSchemes = "Bearer")]
+	public class FeriasController : BaseController
+	{
+		private readonly IMapper _mapper;
+		private readonly IFeriasService _feriasService;
 
-        public FeriasController(IMapper mapper, IFeriasService feriasService)
-        {
-            _mapper = mapper;
-            _feriasService = feriasService;
-        }
+		public FeriasController(IMapper mapper, IFeriasService feriasService)
+		{
+			_mapper = mapper;
+			_feriasService = feriasService;
+		}
 
-        [HttpPost("Register")]
-        public async Task<IActionResult> Register([FromBody] RegisterFeriasViewModel model)
-        {
-            return Result(await _feriasService.CreateFerias(_mapper.Map<FeriasDTO>(model)));
-        }
+		[HttpPost("Register")]
+		public async Task<IActionResult> Register([FromBody] RegisterFeriasViewModel model)
+		{
+			return Result(await _feriasService.CreateFerias(_mapper.Map<FeriasDTO>(model)));
+		}
 
-        [HttpPut("Update")]
-        public async Task<IActionResult> Update([FromBody] UpdateFeriasViewModel model)
-        {
-            return Result(await _feriasService.UpdateFerias(_mapper.Map<FeriasDTO>(model)));
-        }
+		[HttpPut("Update")]
+		public async Task<IActionResult> Update([FromBody] UpdateFeriasViewModel model)
+		{
+			return Result(await _feriasService.UpdateFerias(_mapper.Map<FeriasDTO>(model)));
+		}
 
 
-        [HttpGet("GetById")]
-        public async Task<IActionResult> Get(string id)
-        {
-            return Result(await _feriasService.Get(id));
-        }
+		[HttpGet("GetById")]
+		public async Task<IActionResult> Get(string id)
+		{
+			return Result(await _feriasService.Get(id));
+		}
 
-        [HttpGet("GetAll")]
-        public async Task<IActionResult> GetAll([FromQuery] string? userId)
-        {
-            return Result(await _feriasService.GetAllFerias(userId));
-        }
+		[HttpGet("GetAll")]
+		public async Task<IActionResult> GetAll([FromQuery] string? userId)
+		{
+			return Result(await _feriasService.GetAllFerias(userId));
+		}
 
-        [HttpDelete("Disable")]
-        public async Task<IActionResult> Delete(string id)
-        {
-            return Result(await _feriasService.DisableFerias(id));
-        }
+		[HttpDelete("Disable")]
+		public async Task<IActionResult> Delete(string id)
+		{
+			return Result(await _feriasService.DisableFerias(id));
+		}
 
-        [HttpPatch("Status")]
-        public async Task<IActionResult> Status(string id, Enumerators.Status status)
-        {
-            return Result(await _feriasService.UpdateStatus(id, status));
-        }
+		[HttpPatch("Status")]
+		public async Task<IActionResult> Status(string id, Enumerators.Status status)
+		{
+			return Result(await _feriasService.UpdateStatus(id, status));
+		}
 
-    }
+	}
 }
